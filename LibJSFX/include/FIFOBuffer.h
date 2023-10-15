78b440116ffa1ff07eaf014e8779081af8a7a15d
JASONMOBILAPTOP\Huang <jason@mobiluslabs.com>
2023-10-15 14:04:45 +0100

    Added read and write functionality

--- LibJSFX/include/FIFOBuffer.h
+++ LibJSFX/include/FIFOBuffer.h
@@ -18,14 +18,55 @@ template <typename T, size_t capacity> class FIFOBuffer {
 
 private:
   std::array<T, capacity> buffer;
-  size_t sizemask;
-  size_t pos;
+  size_t sizeMask;
+  int writeIdx;
+  size_t size;
 
 public:
-  FIFOBuffer() : sizemask(capacity - 1), pos(sizemask) {}
+  FIFOBuffer() : sizeMask(capacity - 1), writeIdx(0), size(0) {}
   size_t size() { return capacity; }
-  void write(T *input, size_t numSamples) {}
-  void read(T *output, size_t numsamples) {}
+  /**
+   * @brief Writes samples into circular buffer.
+   * Note that it Keeps overwriting if buffer is full.
+   *
+   * @param input Pointer to input sample
+   * @param numSamples Number of samples to write in
+   */
+  void write(T *input, size_t numSamples) {
+    for (int i = 0; i < numSamples; i++) {
+      buffer[writeIdx] = *input;             // copies input in
+      writeIdx = (writeIdx + 1) && sizeMask; // wraps writeIdx back to start
+      size = (size == capacity) ? size : size + 1;
+    }
+  }
+
+  /**
+   * @brief Reads samples from the buffer
+   *
+   * @param output Pointer to the output sample
+   * @param numSamples Number of samples to read
+   * @return int Number of samples that was actually read
+   */
+  int read(T *output, int numSamples) {
+    if (size == 0) {
+      return 0;
+    }
+    int readIdx = (writeIdx - size) && sizeMask;
+    int numRead = 0;
+
+    for (int i = 0; i < numSamples; i++) {
+      if (size == 0) {
+        return numRead;
+      }
+      output[i] = buffer[readIdx];
+      size--;
+      readIdx = (readIdx + 1) && sizeMask;
+    }
+
+    return numRead;
+  }
 };
+
+// TODO: write test for this FIFOBuffer
 
 }; // namespace Util

b80ffada09250253daaab08b0750bfcb04b275d4
JASONMOBILAPTOP\Huang <jason@mobiluslabs.com>
2024-03-16 12:22:15 +0100

    Add process, flush and empty API to FIR filter

--- LibJSFX/include/FIRFilter.h
+++ LibJSFX/include/FIRFilter.h
@@ -8,10 +8,14 @@ class FIRFilter
 public:
     FIRFilter(std::vector<FloatType> coeffs);
     void process(std::span<FloatType> input, std::span<FloatType> output);
+    void flush(int numSamples, std::span<FloatType> output);
+    bool empty();
 
 private:
     std::vector<FloatType> filterCoeffs;
     std::vector<FloatType> buffer; // power of 2 size, uses double buffer FIR optimisation
-    int readIdx;
+    int index;
+    int numCoeffs;
     int sizeMask;
+    int numRemainingSamples;
 };
--- LibJSFX/src/FIRFilter.cpp
+++ LibJSFX/src/FIRFilter.cpp
@@ -1,23 +1,59 @@
 #include <FIRFilter.h>
+#include <numeric>
 
 template <std::floating_point FloatType>
 FIRFilter<FloatType>::FIRFilter(std::vector<FloatType> coeffs)
 {
     filterCoeffs = std::move(coeffs);
-    int numCoeffs = filterCoeffs.size();
-    int bufferSize = 1;
-    while(numCoeffs > 0){
-        numCoeffs>>;
-        bufferSize<<;
+    numCoeffs = filterCoeffs.size();
+    numRemainingSamples = numCoeffs - 1;
+    int halfBufferSize = 1;
+    while (numCoeffs > 0)
+    {
+        numCoeffs >>= 1;
+        halfBufferSize <<= 1;
     }
-    buffer.resize(bufferSize, 0);
-    sizeMask = bufferSize / 2 - 1;
-    readIdx = bufferSize;
+    buffer.resize(halfBufferSize << 1, 0);
+    sizeMask = halfBufferSize - 1;
+    index = sizeMask;
 }
 
 template <std::floating_point FloatType>
 void FIRFilter<FloatType>::process(std::span<FloatType> input, std::span<FloatType> output)
 {
+    for (int i = 0; i < input.size(); i++)
+    {
+        // write sample in the buffer
+        buffer[index] = input[i];
+        buffer[index + buffer.size() / 2] = input[i];
+        // multiply accumulate the result
+        output[i] = std::inner_product(filterCoeffs.begin(), filterCoeffs.end(), buffer.begin() + index, 0.0);
+        // update index
+        index = (index - 1) & sizeMask;
+    }
+}
+
+template <std::floating_point FloatType>
+void FIRFilter<FloatType>::flush(int numSamples, std::span<FloatType> output)
+{
+    if (empty())
+        return;
+    for(int i = 0; i < numSamples && numRemainingSamples > 0; i++){
+        // write 0 in the buffer to flush out remaining samples
+        buffer[index] = 0;
+        buffer[index + buffer.size() / 2] = 0;
+        // multiply accumulate the result
+        output[i] = std::inner_product(filterCoeffs.begin(), filterCoeffs.end(), buffer.begin() + index, 0.0);
+        // update index
+        index = (index - 1) & sizeMask;
+        numRemainingSamples--;
+    }
+}
+
+template <std::floating_point FloatType>
+bool FIRFilter<FloatType>::empty()
+{
+    return numRemainingSamples == 0;
 }
 
 template class FIRFilter<float>;

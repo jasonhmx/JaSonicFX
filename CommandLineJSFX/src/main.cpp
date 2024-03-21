6af40dd5ebb58675ba2c2e7b7a12221f94821128
JasonHMX <jasonhuang19990427@gmail.com>
2024-03-21 00:01:54 +0000

    Add wav2xml script for impulse response, load into FilterCoeffs struct.
    Next step is to configure debug and construct filter object

--- CommandLineJSFX/src/main.cpp
+++ CommandLineJSFX/src/main.cpp
@@ -1,6 +1,13 @@
 #include "CLI11.hpp"
 #include "FIRFilter.h"
 #include "pugixml.hpp"
+#include "iostream"
+#include "fstream"
+
+struct FilterCoeffs {
+    std::vector<float> a_k;
+    std::vector<float> b_k;
+};
 
 int main(int argc, char** argv)
 {
@@ -14,5 +21,32 @@ int main(int argc, char** argv)
     filter_process_cmd->add_option("--output", ofpath, "Path to output file");
     filter_process_cmd->add_option("--coeffs", coeffpath, "Path to filter coefficients");
     CLI11_PARSE(app, argc, argv);
+
+    if (filter_process_cmd->parsed()) {
+        FilterCoeffs coeffs;
+
+        pugi::xml_document doc;
+        pugi::xml_parse_result result = doc.load_file(coeffpath.c_str());
+        if (!result) {
+            std::cerr << "Error loading XML file: " << result.description() << std::endl;
+            return 1;
+        }
+
+        // Accessing attributes
+        pugi::xml_node root = doc.child("FilterCoeffs");
+        int sample_rate = root.attribute("sample_rate").as_int();
+        int num_samples = root.attribute("num_samples").as_int();
+
+        std::cout << "Sample Rate: " << sample_rate << std::endl;
+        std::cout << "Number of Samples: " << num_samples << std::endl;
+        coeffs.b_k.reserve(num_samples);
+
+        // Parsing coefficients from XML
+        for (pugi::xml_node coeff : root.child("b_k").children("coeff")) {
+            coeffs.b_k.push_back(coeff.text().as_float());
+        }
+        
+    }
+
     return 0;
 }

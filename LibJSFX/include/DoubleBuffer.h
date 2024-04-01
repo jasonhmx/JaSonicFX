4733bbbc0c84b70ea539f6f17ccf359f61b05fda
JasonHMX <jasonhuang19990427@gmail.com>
2024-04-01 16:47:20 +0100

    Update on double buffer incomplete

--- LibJSFX/include/DoubleBuffer.h
+++ LibJSFX/include/DoubleBuffer.h
@@ -1,8 +1,12 @@
 #include <atomic>
 #include <array>
+#include <span>
+#include <algorithm>
 #include <optional>
+#include <mutex>
+
 /**
- * @brief A double buffer implementation
+ * @brief A double buffer implementation. One reader and one writer only!
  *
  * @tparam T The type of element the buffer holds
  * @tparam capacity The capacity of each half of the double buffer
@@ -13,48 +17,29 @@ class DoubleBuffer
 public:
     DoubleBuffer() = default;
     bool push(const T &element);
-    std::optional<T> pop();
+    bool read(std::span<T> output);
 
 private:
     std::atomic<int> size{0}; // how many elements are in the buffer currently
-    int writePtr{0};              // where the write pointer is
-    int readPtr{halfCapacity};
+    int writePtr{0};          // where the write pointer is
     std::atomic<int> readStart{0};
-    std::atomic_flag writeDone{false};
-    std::atomic_flag readDone{false};
+    std::atomic_flag ready{false};
     std::array<T, halfCapacity << 1> buffer;
 };
 
 template <typename T, size_t halfCapacity>
 inline bool DoubleBuffer<T, halfCapacity>::push(const T &element)
 {
-    if (readDone){
-        return false;
-    }
-    buffer[writePtr] = element;
-    size++;
-    writePtr++;
-    if (writePtr == halfCapacity)
-    {
-        ready.test_and_set();
-        readStart = 0;
-    }
-    if (writePtr == halfCapacity * 2)
-    {
-        ready.test_and_set();
-        writePtr = 0;
-        readStart = halfCapacity;
-    }
-    return true;
 }
 
 template <typename T, size_t halfCapacity>
-inline std::optional<T> DoubleBuffer<T, halfCapacity>::pop()
+inline bool DoubleBuffer<T, halfCapacity>::read(std::span<T> output)
 {
     if (!ready)
     {
-        return std::optional<T>();
+        return false;
     }
-    
-
+    ready = false;
+    auto startIt = buffer.begin() + readStart;
+    std::copy(startIt, startIt + halfCapacity, output);
 }
